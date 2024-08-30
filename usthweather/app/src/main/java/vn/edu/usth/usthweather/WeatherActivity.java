8cb1dafce833f30bf737bc70935350d56ff03fd5
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-08-30 14:38:04 +0700

    docs: add usthweather file

---
+++ usthweather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,54 @@
+package vn.edu.usth.usthweather;
+
+import android.util.Log;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("USTHWeather", "onCreate called");
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            ForecastFragment firstFragment = new ForecastFragment();
+            getSupportFragmentManager().beginTransaction().add(
+                    R.id.main, firstFragment).commit();
+            return insets;
+        });
+}
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.i("USTHWeather", "onStart called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.i("USTHWeather", "onResume called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.i("USTHWeather", "onPause called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.i("USTHWeather", "onStop called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i("USTHWeather", "onDestroy called");
+    }
+}
+
