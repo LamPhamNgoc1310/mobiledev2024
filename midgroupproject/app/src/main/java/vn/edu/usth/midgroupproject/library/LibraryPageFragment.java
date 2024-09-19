923ec00542481327b3c65d7ba14acbf34e2cd22f
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2024-09-19 11:23:11 +0700

    docs: add library, liked, signin, songs fragments feat: add play button feature, like button feature, navigation bar feature and playlist onclick feature

--- midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/library/LibraryPageFragment.java
+++ midgroupproject/app/src/main/java/vn/edu/usth/midgroupproject/library/LibraryPageFragment.java
@@ -1,19 +1,25 @@
-package vn.edu.usth.midgroupproject;
+package vn.edu.usth.midgroupproject.library;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import vn.edu.usth.midgroupproject.R;
+import vn.edu.usth.midgroupproject.liked.LikedSongsPageFragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link YourPlaylistFragment#newInstance} factory method to
+ * Use the {@link LibraryPageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class YourPlaylistFragment extends Fragment {
+public class LibraryPageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +30,7 @@ public class YourPlaylistFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public YourPlaylistFragment() {
+    public LibraryPageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +40,11 @@ public class YourPlaylistFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment YourPlaylistFragment.
+     * @return A new instance of fragment LibraryPageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static YourPlaylistFragment newInstance(String param1, String param2) {
-        YourPlaylistFragment fragment = new YourPlaylistFragment();
+    public static LibraryPageFragment newInstance(String param1, String param2) {
+        LibraryPageFragment fragment = new LibraryPageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +65,24 @@ public class YourPlaylistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_your_playlist, container, false);
+        View view = inflater.inflate(R.layout.fragment_library_page, container, false);
+
+        LinearLayout linearLayout1 = view.findViewById(R.id.liked_playlist);
+
+        linearLayout1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateToFragment(new LikedSongsPageFragment());
+            }
+        });
+
+        return view;
+    }
+
+    public void navigateToFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.library_page, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
